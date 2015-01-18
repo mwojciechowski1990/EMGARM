8f5a93cad0b66d88bd020af7944e24881ed0e10b
mwojciechowski1990 <mwojciechowski1990@gmail.com>
2015-01-18 17:21:46 +0100

    first changes to have logs

--- UI/websockets/websocket-server.py
+++ UI/websockets/websocket-server.py
@@ -10,37 +10,50 @@ import Queue
 import serialHandler
 #import time 
 
+logMode = False
 
 readQueue = Queue.Queue(maxsize=1)
 writeQueue = Queue.Queue(maxsize=1)
 
 class dataOutHtmlHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../dataout.html").generate())
 
 class dataOutJSHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../dataout.js").generate())
 
 class smoothieJSHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../smoothie.js").generate())
 
 class mainCSSHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../main.css").generate())
 
 class calibratePIDHTMLHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../calibratepid.html").generate())
 
 class calibratePIDJSHandler(tornado.web.RequestHandler):
   def get(self):
+    global logMode
+    logMode = False
     loader = tornado.template.Loader(".")
     self.write(loader.load("../calibratepid.js").generate())
 
@@ -51,28 +64,37 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     print 'connection opened...'
     #self.write_message("The server says: 'Hello'. Connection was accepted.")
     self.run = True
-    ui_thread = threading.Thread(target=self.update_ui, args=(readQueue, ))
-    ui_thread.daemon = True
-    ui_thread.start()
+    self.logFile = open('logFile.log', 'r+')
+    if not logMode:
+      ui_thread = threading.Thread(target=self.update_ui, args=(readQueue, ))
+      ui_thread.daemon = True
+      ui_thread.start()
     serial_thread = serialHandler.serialHandler(readQueue, writeQueue)
     serial_thread.daemon = True
     serial_thread.start()
 
   def on_message(self, message):
     print 'received:', message
+    self.logFile.write(message + '\n')
     writeQueue.put(message)
+    if logMode:
+      update_ui(readQueue)
 
   def on_close(self):
     print 'connection closed...'
     self.run = False
     writeQueue.put('exit')
+    self.logFile.close()
 
   def update_ui(self, rQ):
-    while self.run:
+    while self.run and not logMode:
       if not rQ.empty():
         val = rQ.get()
         #print val
         self.write_message(str(val))
+    if logMode:
+      logs = self.logFile.read()
+      self.write_message(logs)
       
 
 application = tornado.web.Application([
